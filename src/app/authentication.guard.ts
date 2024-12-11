a9bb7d170b46bcec79ddc8578a4a2b9cb9f08c04
Bairaboina Vyshnavi <vyshnavibairaboina@gmail.com>
2024-12-11 22:47:30 +0530

    localstorage fix

--- src/app/authentication.guard.ts
+++ src/app/authentication.guard.ts
@@ -1,9 +1,12 @@
+import { isPlatformBrowser } from '@angular/common';
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authenticationGuard: CanActivateFn = (route, state) => {
-  const router = inject(Router); // Inject the Router
-  const isAdmin = JSON.parse(localStorage.getItem('isAdmin') || 'null');
+  const router = inject(Router);
+  let isAdmin;
+  if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+   isAdmin = JSON.parse(localStorage.getItem('isAdmin') || 'null');
   if((isAdmin === true || isAdmin === false)){
     return true;
   }
@@ -11,4 +14,8 @@ export const authenticationGuard: CanActivateFn = (route, state) => {
     router.navigate(['/login']);
     return false;
   }
+}else {
+  router.navigate(['/login']);
+    return false;
+}
 };
